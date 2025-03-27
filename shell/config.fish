b66601b59b4d027ae1d0eb5053519435e0d10b82
codayon <reshadahammedayon@proton.me>
2025-03-27 20:52:04 +0600

    replace greetings with fastfetch

--- shell/config.fish
+++ shell/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -g -x fish_greeting (set_color brgreen)"check "(set_color brcyan)"your "(set_color brred)"to-do "(set_color normal)"list!"
+    fastfetch
+    set -g -x fish_greeting ""
+    # set -g -x fish_greeting (set_color brgreen)"check "(set_color brcyan)"your "(set_color brred)"to-do "(set_color normal)"list!"
 end
