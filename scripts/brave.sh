75900b045443ff5c41a718207a460948bae8461c
codayon <reshadahammedayon@proton.me>
2025-08-05 16:25:09 +0600

    fix(cleanup): remove scripts

--- scripts/brave.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run Brave with specified flags
-brave --disable-gpu --enable-unsafe-swiftshader
--- scripts/chromium.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run Brave with specified flags
-chromium --disable-gpu --enable-unsafe-swiftshader
